2813b9a4d0c445dff1cbf9c8cee7ff7cc9abc1a5
Luciano Luzzi <luciano.luzzi@nl.abnamro.com>
2020-06-02 12:20:22 +0200

    Making kotlin classes mockable for mockito. Providing unit test for api provider.

--- network/src/test/java/com/lucianoluzzi/networkbuilder/APIProviderTest.kt
+++ network/src/test/java/com/lucianoluzzi/networkbuilder/APIProviderTest.kt
@@ -1,7 +1,18 @@
 package com.lucianoluzzi.networkbuilder
 
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.jupiter.api.Test
 import retrofit2.Retrofit
 
 class APIProviderTest {
-    //val retrofit = mock<Retrofit>()
+    val retrofit = mock<Retrofit>()
+
+    @Test
+    fun `verify retrofit called with correct class when createService`() {
+        APIProvider(retrofit).createService(MockInterface::class.java)
+        verify(retrofit).create(MockInterface::class.java)
+    }
+
+    private interface MockInterface
 }
